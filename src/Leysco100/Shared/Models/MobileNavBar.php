889a058fcb5ef740da6908713d4b8c62efb119ff
bereank <bereankibet@gmail.com>
2023-07-23 20:11:23 +0300

    Using tenanct connection on models

--- src/Leysco100/Shared/Models/MobileNavBar.php
+++ src/Leysco100/Shared/Models/MobileNavBar.php
@@ -1,12 +1,13 @@
 <?php
 namespace Leysco100\Shared\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
 class MobileNavBar extends Model
 {
-    use HasFactory;
+    use HasFactory,UsesTenantConnection;
     protected $table = "mobile_nav_bars";
     protected $guarded = [];
 }
