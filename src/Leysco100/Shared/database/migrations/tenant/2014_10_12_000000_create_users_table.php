8a851d62b01e013e199ac2b4048e67b627b46c2c
bereank <bereankibet@gmail.com>
2023-09-14 12:27:12 +0300

    Mapping user fields

--- src/Leysco100/Shared/database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ src/Leysco100/Shared/database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -40,6 +40,7 @@ class CreateUsersTable extends Migration
             $table->integer('status')->default(1)->comment("Active: 0=Inactive, 1=Active, 2=Suspended");
             $table->integer('useLocalSearch')->default(0)->comment("0=No, 1=Yes");
             $table->string('localUrl')->nullable();
+            $table->integer('gate_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
