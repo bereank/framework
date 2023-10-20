449b5eed40d524abdb820b0c4b1a018728de1b78
robertwac <robertkimaru1998@gmail.com>
2023-10-20 16:10:56 +0300

    Query manager models update

--- src/Leysco100/Shared/Models/OUTB.php
+++ src/Leysco100/Shared/Models/OUTB.php
@@ -8,7 +8,31 @@ use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
 class OUTB extends Model
 {
-    use HasFactory,UsesTenantConnection;
-    protected $table= 'o_u_t_b';
+    use HasFactory, UsesTenantConnection;
+    protected $table = 'o_u_t_b';
     protected $guarded = [];
+
+    protected $appends = array('object');
+
+    public function getObjectAttribute()
+    {
+        if ($this->ObjectType == 0) {
+            return "No Object";
+        }
+
+        if ($this->ObjectType == 1) {
+            return "Master Data";
+        }
+
+        if ($this->ObjectType == 2) {
+            return "Master Data Rows";
+        }
+        if ($this->ObjectType == 3) {
+            return "Document";
+        }
+
+        if ($this->ObjectType == 4) {
+            return "Document Rows";
+        }
+    }
 }
