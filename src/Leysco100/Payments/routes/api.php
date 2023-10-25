b6704ab5f75c34389dee7fae1deb5cc0f2da7400
robertwac <robertkimaru1998@gmail.com>
2023-10-25 12:56:34 +0300

    KCB payments api's

--- src/Leysco100/Payments/routes/api.php
+++ src/Leysco100/Payments/routes/api.php
@@ -6,4 +6,9 @@ use Illuminate\Support\Facades\Route;
 use Leysco100\Payments\Http\Controllers\API\PaymentsController;
 
 
+
+
+
+Route::post('kcb/payments/notification', [PaymentsController::class, 'paymentNotification'])->withoutMiddleware(['auth:sanctum']);
+Route::post('kcb/payments/query', [PaymentsController::class, 'paymentQuery'])->withoutMiddleware(['auth:sanctum']);
 Route::apiResources(['payments' => PaymentsController::class]);
