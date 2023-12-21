150e7c18ba6cad3604d874360a0acab6d0747346
KamauAdrian <adriankubai1999@gmail.com>
2023-12-21 11:31:52 +0300

    updated user defaults

---
+++ src/Leysco100/Administration/database/migrations/tenant/2023_12_21_105801_add_new_columns_to_tables.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('o_u_d_g_s', function (Blueprint $table) {
+            if (!Schema::hasColumn('o_u_d_g_s', 'DftBinLoc')) {
+                $table->string('DftBinLoc')->nullable()->after('AddToFavourites');
+            }
+            if (!Schema::hasColumn('o_u_d_g_s', 'ClockIn')) {
+                $table->string('ClockIn')->nullable()->after('DftBinLoc');
+            }
+            if (!Schema::hasColumn('o_u_d_g_s', 'EtstCode')) {
+                $table->string('EtstCode')->nullable()->after('ClockIn');
+            }
+            if (!Schema::hasColumn('o_u_d_g_s', 'RouteID')) {
+                $table->integer('RouteID')->nullable()->after('EtstCode');
+            }
+            if (!Schema::hasColumn('o_u_d_g_s', 'RouteActive')) {
+                $table->integer('RouteActive')->nullable()->after('RouteID');
+            }
+            if (!Schema::hasColumn('o_u_d_g_s', 'SellFromBin')) {
+                $table->string('SellFromBin')->nullable()->after('RouteActive');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('o_u_d_g_s', function (Blueprint $table) {
+            $table->dropColumn('DftBinLoc');
+            $table->dropColumn('EtstCode');
+            $table->dropColumn('ClockIn');
+            $table->dropColumn('RouteID');
+            $table->dropColumn('RouteActive');
+            $table->dropColumn('SellFromBin');
+        });
+    }
+};
