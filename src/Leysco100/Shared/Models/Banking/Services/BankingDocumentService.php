ca5bff7962b36c11685aa46dbe314281ab7454f1
KamauAdrian <adriankubai1999@gmail.com>
2024-01-16 07:20:15 +0300

    documents debug end

--- src/Leysco100/Shared/Models/Banking/Services/BankingDocumentService.php
+++ src/Leysco100/Shared/Models/Banking/Services/BankingDocumentService.php
@@ -81,8 +81,6 @@ class BankingDocumentService
             'PaidToDate' => $document->PaidToDate ? $document->PaidToDate : $request['TotalPaid'],
         ];
 
-        dd($InvoiceDetails);
-
         $document->update($InvoiceDetails);
         if (count($request['cheques']) > 0) {
             //Saving RCT1  Chekc Details
