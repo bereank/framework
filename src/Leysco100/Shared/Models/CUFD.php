889a24cff962db6b33c794f3f3a0a9307e40ffa7
robertwac <robertkimaru1998@gmail.com>
2024-02-29 16:15:22 +0300

    Bin stock check update

--- src/Leysco100/Shared/Models/CUFD.php
+++ src/Leysco100/Shared/Models/CUFD.php
@@ -4,6 +4,7 @@ namespace Leysco100\Shared\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Leysco100\Shared\Models\Shared\Models\APDI;
 use Leysco100\Shared\Models\Shared\Models\UFD1;
 use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
@@ -17,5 +18,10 @@ class CUFD extends Model
     public function items()
     {
         return $this->hasMany(UFD1::class, 'FieldID');
+    }
+
+    public function objtype()
+    {
+        return $this->belongsTo(APDI::class, 'ObjType', 'ObjectID');
     }
 }
